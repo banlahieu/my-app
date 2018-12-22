abfce129148fbf2c49c436d592acaccc7e6fe839
banlahieu <banlahieu@gmail.com>
2018-12-22 09:40:25 +0700

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { TitleComponent } from './title/title/title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
--- src/app/title/title.module.ts
+++ src/app/title/title.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { TitleRoutingModule } from './title-routing.module';
 import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [TitleComponent],
   imports: [
-    CommonModule,
-    TitleRoutingModule
+    CommonModule
   ]
 })
 export class TitleModule { }
