9cd20b93588487b7663737a704cae45da3815807
banlahieu <banlahieu@gmail.com>
2018-12-22 09:44:40 +0700

    updated

--- src/app/title/title.module.ts
+++ src/app/title/title.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TitleComponent } from './title/title.component';
 
 @NgModule({
-  declarations: [TitleComponent],
+  declarations: [],
   imports: [
     CommonModule
   ]
